cd91523b4bcfec8b0b5017689bf6be63145e5c62
Stanley1111 <stanley.tyl@gmail.com>
2018-12-03 22:00:27 +0000

    basic image download done

--- download_avatars.js
+++ download_avatars.js
@@ -17,16 +17,31 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
+function downloadImageByURL(url, filePath) {
+  // ...
+  var fs = require('fs');
+
+  request.get(url)
+         .on('error', function (err) {
+           throw err;
+         })
+         .on('response', function (response) {
+           console.log('Response Status Code: ', response.statusCode);
+         })
+         .pipe(fs.createWriteStream(filePath));
+}
+
+//Testing
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+
+// getRepoContributors("jquery", "jquery", function(err, result) {
+//   console.log("Errors:", err);
+
+//   let avatars = JSON.parse(result);
+//   avatars.forEach(function(item) {
+//     console.log(item.avatar_url);
+//   });
 
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-
-  let avatars = JSON.parse(result);
-  avatars.forEach(function(item) {
-    console.log(item.avatar_url);
-  });
-
-
-});
+// });
 
