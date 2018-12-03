1e6bdcdd5504d8d8ef44efb8ca980900d33e21b8
Stanley1111 <stanley.tyl@gmail.com>
2018-12-03 21:48:39 +0000

    printed the avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -21,6 +21,12 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+
+  let avatars = JSON.parse(result);
+  avatars.forEach(function(item) {
+    console.log(item.avatar_url);
+  });
+
+
 });
 
